f0b626e1af773a7445e6c23a53731abca0ccbf64
goose <tochenyuxiao@163.com>
2022-09-23 23:36:43 +0800

    feat: arr2tree

--- src/arr2tree/index.js
+++ src/arr2tree/index.js
@@ -2,14 +2,14 @@
  * 数组转树形结构
  */
 const arr = [
-    { id: 2, name: '部门B', parentId: 0},
-    { id: 3, name: '部门C', parentId: 1},
-    { id: 1, name: '部门A', parentId: 2},
-    { id: 4, name: '部门D', parentId: 1},
-    { id: 5, name: '部门E', parentId: 2},
-    { id: 6, name: '部门F', parentId: 3},
-    { id: 7, name: '部门G', parentId: 2},
-    { id: 8, name: '部门H', parentId: 4}
+    { id: 2, name: '部门B', parentId: 0 },
+    { id: 3, name: '部门C', parentId: 1 },
+    { id: 1, name: '部门A', parentId: 2 },
+    { id: 4, name: '部门D', parentId: 1 },
+    { id: 5, name: '部门E', parentId: 2 },
+    { id: 6, name: '部门F', parentId: 3 },
+    { id: 7, name: '部门G', parentId: 2 },
+    { id: 8, name: '部门H', parentId: 4 }
 ]
 
 function arr2tree(arr) {
@@ -18,7 +18,7 @@ function arr2tree(arr) {
     const idMapping = {}
     const result = []
     const arrCopy = arr.map((item = {}) => {
-        const itemCopy = {...item}
+        const itemCopy = { ...item }
         idMapping[item.id] = itemCopy
         return itemCopy
     });
